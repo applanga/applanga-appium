ace21f0046260bf5317f35bfde4d930b757b12fb
Leonard Arnold <leonard.arnold1@gmail.com>
2023-07-19 17:05:25 +0200

    improved screenshot upload and method handling

--- applanga-appium.js
+++ applanga-appium.js
@@ -1,214 +1,193 @@
-var parseString = require('xml2js').parseString;
+var parseStringPromise = require('xml2js').parseStringPromise;
 var fs = require('fs');
-var request = require('request');
+var axios = require('axios');
+var sizeOf = require('image-size');
 
-const apiUrl = "https://api.applanga.com/v1/api/screenshots?app="
+const apiUrl = 'https://api.applanga.com/v1/api/screenshots?app=';
 
-var client
-var language 
-var platform
-var tag
-var appId
-var apiToken
+async function captureScreenshot(driver, tagName, applangaJsonPath) {
+  apiToken = getAPIToken(applangaJsonPath);
+  appId = getAppID(applangaJsonPath);
 
-async function captureScreenshot(theClient, theTag, thePlatform, theLanguage, theAppId, theApiToken)
-{
-    client = theClient
-    tag = theTag
-    platform = thePlatform
-    language = theLanguage
-    appId = theAppId
-    apiToken = theApiToken
-    await doScreenshot()
+  let screenshotLocation = __dirname + '/tmpScreen.png';
+  var dimensions;
+  try {
+    screenshot = await driver.takeScreenshot();
+    await fs.promises.writeFile(screenshotLocation, screenshot, 'base64');
+  } catch (error) {
+    console.log('Error ocurred while saving screenshot: ' + error);
+  }
+
+  const screenshotDimensions = sizeOf(screenshotLocation);
+
+  let xml = await driver.getPageSource();
+  let parsedXml = await parseStringPromise(xml);
+  let allTexts = getAllTextOnScreen(driver, parsedXml);
+  let positions = await getAllTextPositions(
+    driver,
+    allTexts,
+    screenshotDimensions
+  );
+
+  await doUpload(
+    driver,
+    tagName,
+    apiToken,
+    appId,
+    positions,
+    screenshotLocation
+  );
 }
 
-async function doScreenshot()
-{
-    let xml = await client.getPageSource()
-    parseString(xml,async function (err, result) {
-        console.log(JSON.stringify(result))
-        let allTexts = getAllTextOnScreen(result)
-        await uploadTextPositions(allTexts)
+function getDeviceLanguageLong(client) {
+  let country = client.isAndroid
+    ? client.capabilities.desired.locale
+    : client.capabilities.locale;
+  let language = client.isAndroid
+    ? client.capabilities.desired.language
+    : client.capabilities.language;
+  if (!country) return language;
+  else return language + '-' + country;
+}
+
+function getAllTextOnScreen(driver, object) {
+  var foundTexts = [];
+  for (var key in object) {
+    if (object.hasOwnProperty(key)) {
+      let value = object[key];
+      if (typeof value == 'object') {
+        foundTexts = foundTexts.concat(getAllTextOnScreen(driver, value));
+      }
+      var text;
+      for (let i = 0; i < value.length; i++) {
+        if (value[i]['$'] === undefined) continue;
+        if (driver.isAndroid) {
+          text = value[i]['$']['text'];
+        } else {
+          text = value[i]['$']['label'];
+        }
+        if (text !== undefined && text != null && text !== '') {
+          foundTexts.push(text);
+        }
+      }
+    }
+  }
+  return foundTexts;
+}
+
+async function getAllTextPositions(client, allTexts, screenshotDimensions) {
+  var positions = [];
+  var ratio = 1;
+  if (!client.isAndroid) {
+    // get the ratio for iOS devices
+    // get screen size
+    const screen = await client.$(`//XCUIElementTypeApplication`);
+    let screenPosition = await screen.getLocation();
+    let screenSize = await screen.getSize();
+    if (screenPosition.x != 0 || screenPosition.y != 0) {
+      console.error(
+        'Screen position is not 0,0. This is not supported by Applanga. Please make sure your app is fullscreen.'
+      );
+    }
+    ratio = screenshotDimensions.width / screenSize.width;
+  } 
+  for (let i = 0; i < allTexts.length; i++) {
+    const textValue = allTexts[i];
+    var element;
+    if (client.isAndroid) {
+      const selector = 'new UiSelector().text("' + textValue + '")';
+      element = await client.$(`android=${selector}`);
+    } else {
+      const selector = `label == '` + textValue + `'`;
+      element = await client.$(`-ios predicate string:${selector}`);
+    }
+
+    let elementPosition = await element.getLocation();
+    let elementSize = await element.getSize();
+    positions.push({
+      text: textValue,
+      x: elementPosition.x * ratio,
+      y: elementPosition.y * ratio,
+      width: elementSize.width * ratio,
+      height: elementSize.height * ratio,
     });
-    await client.pause(4000)
+  }
+  return positions;
 }
 
-function getAllTextOnScreen(object)
-{
-    var foundTexts = []
-    for (var key in object) {
-        if (object.hasOwnProperty(key)) {
-            let value = object[key]
-            if(typeof(value) == "object")
-            {
-                foundTexts = foundTexts.concat(getAllTextOnScreen(value))
-            }
-            if(key.includes("TextView"))
-            {
-                for (let i = 0; i < value.length; i++) {
-                    const text = value[i]["$"]["text"]
-                    if(text != null)
-                    {
-                        foundTexts.push(text)
-                    }
-                }
-            }
-            if(key.includes("XCUIElementTypeStaticText"))
-            {
-                for (let i = 0; i < value.length; i++) {
-                    const text = value[i]["$"]["value"]
-                    if(text != null)
-                    {
-                        foundTexts.push(text)
-                    }
-                }
-            }
+async function doUpload(
+  client,
+  tagName,
+  apiToken,
+  appId,
+  stringPositions,
+  imageLocation
+) {
+  let screenSize = await client.getWindowRect();
+  let opts = client.capabilities;
+  var data = {
+    screenTag: tagName,
+    width: screenSize.width,
+    height: screenSize.height,
+    deviceModel: opts.deviceModel || 'appium',
+    platform: opts.platformName,
+    operatingSystem: opts.platformVersion || opts.platform,
+    bundleVersion: 1,
+    deviceLanguageLong: getDeviceLanguageLong(client),
+    isBlank: false,
+    useOCR: false,
+    stringPositions: stringPositions,
+  };
+  const form = {
+    data: JSON.stringify(data),
+    file: fs.createReadStream(imageLocation),
+  };
 
-        }
+  const headers = {
+    Authorization: apiToken,
+    'Content-Type': 'multipart/form-data',
+  };
+  const fullUrl = apiUrl + appId;
+  try {
+    let res = await axios.post(fullUrl, form, { headers: headers });
+    if (res.status != 200) {
+      console.error(
+        'Applanga Screenshot upload failed with status code: ',
+        res.status + ', msg: ' + res.data.message
+      );
+    } else {
+      console.log('Applanga Screenshot Upload successful!');
     }
-    return foundTexts
+  } catch (err) {
+    console.error('Applanga Screenshot upload failed:', err);
+  }
 }
 
-async function uploadTextPositions(allTexts)
-{
-    var positions = []
-    for (let i = 0; i < allTexts.length; i++) {
-        const text = allTexts[i];
-        var position
-        if(platform == "Android")
-        {
-            position = await getAndroidStringPosition(text)
-        }
-        else
-        {
-            position = await getIosStringPosition(text)
-        }
-        
-        positions.push(position)
-    }
-    let screenshot = await client.takeScreenshot();
-    let screenshotLocation = __dirname + "/test.png"
-    fs.writeFile(screenshotLocation, screenshot, 'base64',async function(error) {
-        if(error!=null)
-        {
-            console.log('Error occured while saving screenshot' + error);
-        }else
-        {
-            let screenSize = await client.getWindowRect();
-            await doUpload(screenSize.width,screenSize.height,positions,screenshotLocation);
-        }
-    });
+// returns the api token in the format "Bearer <token>"
+function getAPIToken(path) {
+  if (!path) path = '';
+
+  let applangaJson = JSON.parse(fs.readFileSync(path + '.applanga.json'));
+  let apiKey = applangaJson.app.access_token;
+
+  if (apiKey.length != 57) {
+    console.log('Invalid Applanga API token.');
+    return;
+  }
+  return 'Bearer ' + apiKey;
 }
 
-async function getAndroidStringPosition(textValue)
-{
-    const selector = 'new UiSelector().text("' + textValue + '")'
-    const element = await client.$(`android=${selector}`)
-    let elementPosition = await element.getLocation()  
-    let elementSize = await element.getSize()
-	return {text:textValue,x:elementPosition.x,y:elementPosition.y,width: elementSize.width,height: elementSize.height}
+function getAppID(path) {
+  let token = getAPIToken(path);
+
+  // remove Bearer from token
+  let str = token.substring('Bearer '.length);
+  if (str.charAt(24) != '!') {
+    console.log('Invalid Applanga AppID, missing exclamation mark.');
+    return;
+  }
+  var appId = str.substring(0, 24);
+  return appId;
 }
 
-async function getIosStringPosition(textValue)
-{
-    const element = await client.$('~' + textValue)
-    let elementPosition = await element.getLocation()  
-    let elementSize = await element.getSize()
-	return {text:textValue,x:elementPosition.x,y:elementPosition.y,width: elementSize.width,height: elementSize.height}
-}
-
-async function doUpload(screenWidth,screenHeight,stringPositions,imageLocation)
-{
-        let opts = platform == "Android" ? client.capabilities.desired : client.capabilities
-        var data = {
-            screenTag : tag,
-            width: screenWidth,
-            height : screenHeight,
-            deviceModel : opts.deviceName,
-            platform : opts.platformName,
-            operatingSystem : opts.platformVersion,
-            bundleVersion : 1,
-            deviceLanguageLong : language,
-            isBlank : false,
-            useOCR : false,
-            stringPositions : stringPositions
-        }
-        const form = {
-            data: JSON.stringify(data),
-            file: fs.createReadStream(imageLocation)
-        }
-       
-        var headers = {Authorization: apiToken}
-
-        request.post({url: apiUrl + appId, formData: form, headers: headers}, function optionalCallback(err, httpResponse, body) {
-            if (err) {
-                return console.error('Applanga Screenshot upload failed:', err);
-            }
-            console.log('Applanga Screenshot Upload successful!');
-        });
-
-}
-
-function readAPIToken() {
-    let rawdata = fs.readFileSync('.applanga.json');
-   
-    let applanga = JSON.parse(rawdata);  
-    
-    let app = applanga.app;
-    let token = app.access_token
-
-    if (token.length < 57 || token.length > 57)  {
-        console.log("Invalid Applanga API token" + " " + token.length)
-        return
-    }
-
-   
-
-    return token
-    
-}
-
-function validateAndfindAppId(str) {
-
-    let exChar = str.charAt(24);
-    if (exChar != "!") {
-        console.log("Invalid Applanga AppID, missing exclamation mark")
-        return
-    }
-    if (str < 25 || str > 25)  {
-        console.log("Invalid Applanga AppID" + " " + str.length)
-        return
-    }
-
-    var subString = str.substr(0 , 24);
-
-    
-    return subString
-
-}
-
-function finalizeToken(str) {
-
-    let bearer = "Bearer" + " " ;
-
-    let accssTkn = bearer.concat(str);
-
-    return accssTkn
-    
-}
-
-function getAPIToken() {
-    let key = readAPIToken()
-    let token = finalizeToken(key)
-
-    return token
-}
-
-function getAppID() {
-    let token = readAPIToken()
-    let appId = validateAndfindAppId(token)
-
-    return appId
-}
-
-module.exports = {captureScreenshot,getAPIToken,getAppID};
+module.exports = { captureScreenshot };
